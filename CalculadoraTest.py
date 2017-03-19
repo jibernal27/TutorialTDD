1d2f26cbac804bf600a81b9ded716c8773a180ae
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-03-19 18:17:08 -0500

    Se agrega pruebas para sumar multiples números con distintos separadores (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -14,3 +14,5 @@ class CalculadoraTest(TestCase):
         self.assertEqual(Calculadora().sumar("1,3"),4,"Dos números")
     def test_sumar_cadenaConMultiplesNumeros(self):
         self.assertEqual(Calculadora().sumar("5,2,4,1"),12,"Multiples números")
+    def test_sumar_MultiplesNumerosDistintosSeparadores(self):
+        self.assertEqual(Calculadora().sumar("5,2&4:1:2&8"),22,"Multiples números distitnos separadores")
